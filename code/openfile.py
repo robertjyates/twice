385819d7dcd30780a15d589874cbf156667e92f6
Robert Yates <robertjyates@gmail.com>
2014-07-20 19:04:25 -0700

    Updated code to take in a list of dictionary filenames.

--- code/openfile.py
+++ code/openfile.py
@@ -2,24 +2,20 @@ from sys import argv
 from collections import Counter
 
 if (len(argv) != 2):
-    print "Usage: %s <inputstring>" % argv[0]
+    print "Usage: %s <inputStr>" % argv[0]
 else:
-    inputstring = argv[1]
-    print "Input string:", inputstring
-    inputCnt = Counter(inputstring)
-    print list(inputCnt.elements())
-    print
-
-    with open('data.in', 'r') as f:
-        for line in f:
-            word = line.strip()
-            wordCnt = Counter(word)
-            print word
-            print list(wordCnt.elements())
-
-            remainingCnt = wordCnt - inputCnt
-            print list(remainingCnt.elements())
-            if remainingCnt.values() == []:
-                print "Word matches: %s" % word
-            print
+    inputStr = argv[1]
+    inputCnt = Counter(inputStr)
+    print "Input string:", inputStr
+    dictionaryFilenames = ["data.in"]
+    #dictionaryFilenames = ["../scowl-7.1/final/english-words.10",
+    #                  "../scowl-7.1/final/english-words.20"]
+    for filename in dictionaryFilenames:
+        with open(filename, 'r') as f:
+            for line in f:
+                word = line.strip()
+                wordCnt = Counter(word)
+                remainingCnt = wordCnt - inputCnt
+                if remainingCnt.values() == []:
+                    print "Word matches: %s" % word
 
