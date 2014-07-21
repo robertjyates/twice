eb4a7951009103ebe1ff36415753e5a62ac51e2b
Robert Yates <robertjyates@gmail.com>
2014-07-20 18:20:27 -0700

    Converted input string to character list.

--- code/openfile.py
+++ code/openfile.py
@@ -5,6 +5,8 @@ if (len(sys.argv) != 2):
 else:
     inputstring = sys.argv[1]
     print "Input string:", inputstring
+    inputcharlist = list(inputstring)
+    print "Input char list:", inputcharlist
 
     myfile = open( "data.in", "r" )
     mylist = []
