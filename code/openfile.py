4710ea0e9241bdb7f085c3ec0ec30257064528ee
Robert Yates <robertjyates@gmail.com>
2014-07-20 18:16:51 -0700

    Did command line input.

--- code/openfile.py
+++ code/openfile.py
@@ -1,8 +1,16 @@
-myfile = open( "data.in", "r" )
-mylist = []
-for line in myfile:
-    mylist.append( line.strip() )
-myfile.close()
+import sys
 
-for line in mylist:
-    print line
+if (len(sys.argv) != 2):
+    print "Usage: %s <inputstring>" % sys.argv[0]
+else:
+    inputstring = sys.argv[1]
+    print "Input string:", inputstring
+
+    myfile = open( "data.in", "r" )
+    mylist = []
+    for line in myfile:
+        mylist.append( line.strip() )
+    myfile.close()
+  
+    for line in mylist:
+        print line
